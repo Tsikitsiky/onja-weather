5d12b7ad072fa7c9a6f3cd611ce37f9cfd9b323b
Sarah Razanatsoa <sarah.raz@onja.org>
2021-01-18 14:39:21 +0300

    add pointer to the buttons

--- dist/onja-weather.e31bb0bc.js
+++ dist/onja-weather.e31bb0bc.js
@@ -32495,7 +32495,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51264" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58021" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
